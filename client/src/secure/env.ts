fa608bee0cfe200f7cd1a9c01adb0a11147677db
TeaChoco <teachoco.official@gmail.com>
2026-04-11 18:13:17 +0700

    Update env.ts

--- client/src/secure/env.ts
+++ client/src/secure/env.ts
@@ -1,12 +1,14 @@
 // -Path: "PokeRotom/client/src/secure/env.ts"
 
+const MODE = String(import.meta.env.VITE_MODE) || 'development';
 const HOST = String(import.meta.env.VITE_CLIENT_HOST) || '0.0.0.0';
 
 const env = {
-    PORT: Number(import.meta.env.VITE_CLIENT_PORT) || 8000,
+    MODE,
     HOST,
+    PORT: Number(import.meta.env.VITE_CLIENT_PORT) || 8000,
     API_URL:
-        HOST === '0.0.0.0'
+        HOST === '0.0.0.0' && MODE === 'development'
             ? 'http://192.168.1.123:3000'
             : String(import.meta.env.VITE_API_URL) || 'http://127.0.0.1:3000',
     API_TOKEN_KEY: String(import.meta.env.VITE_API_TOKEN_KEY) || '',
