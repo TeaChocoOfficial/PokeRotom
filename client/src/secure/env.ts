a3fe40049ccf532057cf5155da41fce374600c1b
TeaChoco <teachoco.official@gmail.com>
2026-04-11 18:14:39 +0700

    Update env.ts

--- client/src/secure/env.ts
+++ client/src/secure/env.ts
@@ -7,10 +7,7 @@ const env = {
     MODE,
     HOST,
     PORT: Number(import.meta.env.VITE_CLIENT_PORT) || 8000,
-    API_URL:
-        HOST === '0.0.0.0' && MODE === 'development'
-            ? 'http://192.168.1.123:3000'
-            : String(import.meta.env.VITE_API_URL) || 'http://127.0.0.1:3000',
+    API_URL: String(import.meta.env.VITE_API_URL) || 'http://127.0.0.1:3000',
     API_TOKEN_KEY: String(import.meta.env.VITE_API_TOKEN_KEY) || '',
 } as const;
 
